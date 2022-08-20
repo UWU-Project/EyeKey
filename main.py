0fb3a3f28a1be8f158aead4542db8356bdb27bef
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2022-08-20 16:33:56 +0530

    2nd Commit EYE blink detection Done for one eye

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import cv2
 import numpy as np
 import dlib
+from math import hypot
 
 # To capture video from webcam.
 cap = cv2.VideoCapture(0)
-
 # face detector using dlib library
 detector = dlib.get_frontal_face_detector()
-
 # Face landmark shape predictor
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-def midpoint(p1 ,p2):
-    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+font = cv2.FONT_ITALIC
+
+
+def midpoint(p1, p2):
+    return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2)
+
 
 while True:
     # Read the frame
@@ -40,6 +43,16 @@ while True:
         # print Horizontal and Vertical Lines
         hor_line = cv2.line(frame, left_point, right_point, (0, 0, 255), 2)
         ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
+        ver_line_length = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+        hor_line_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+
+        ratio = hor_line_length / ver_line_length
+
+        if ratio > 6:
+            cv2.putText(frame, "EyeBlink", (50, 150), font, 3, (255, 0, 0))
+
+        print(hor_line_length / ver_line_length)
 
     # Display
     cv2.imshow("Frame", frame)
